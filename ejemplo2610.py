fbe1994e045b588e00df8422abb90ce2b8407ca6
javiermilesinacap <109394677+javiermilesinacap@users.noreply.github.com>
2022-10-26 20:22:36 -0300

    Create ejemplo2610.py

---
+++ ejemplo2610.py
@@ -0,0 +1,24 @@
+import abc
+class Vehiculo(abc):
+    def __init__(self):
+        self.ruedas=0
+        self.__color=(255,0,0)
+        self.velocidad=0
+        self.motor=""
+        print("Hola, soy un Vehiculo")
+        pass
+    def getColor(self):
+        print("Soy de color: ",self.__color)
+class Aereo(Vehiculo):
+    def __init__(self, motor="Turbina", velocidad=100):
+        super().__init__()
+        alas=0
+        print("Ahora soy un avión", self.getColor())
+class Anfibio(Vehiculo):
+        profundidadMax=0
+class Terrestre(Vehiculo):
+    pass
+vehiculo = Aereo(motor="Reaccion")
+vehiculoA = Vehiculo()
+#vehiculo.__color="Blanco"
+print(vehiculo.getColor())
